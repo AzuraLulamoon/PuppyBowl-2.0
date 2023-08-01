d7dd6322fe11aa64e1de89b33349d8601bb35a2b
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-08-01 12:27:24 -0500

    filled out some other files, still dealing with the fetch bug

--- Puppybowl2.0/src/components/AllPlayers.jsx
+++ Puppybowl2.0/src/components/AllPlayers.jsx
@@ -1,21 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const baseUrl = 'https://fsa-puppy-bowl.herokuapp.com/api/2306-FTB-ET-WEB-FT/players'
+// const baseUrl = ['https://fsa-puppy-bowl.herokuapp.com/api/2306-FTB-ET-WEB-FT/players']
 
 export default function AllPlayers() {
     
-    const [ AllPlayer, setAllPlayer ] = useState([])
+    const [ players, setPlayers ] = useState([])
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function fetchData() {
             
             try {
-                const response = await fetch(baseUrl)
+                const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2306-FTB-ET-WEB-FT/players')
                 const data = response.json();
 
                 console.log(data)
-                setAllPlayer(data.data.players);
+                setPlayers(data.data.players);
+                
             } catch(err) {
                 console.error(err);
             }
@@ -24,10 +27,12 @@ export default function AllPlayers() {
         fetchData()
     }, [])
 
-    console.log(AllPlayer)
     return <>
-        {AllPlayer.map((p, index) => <div key={index} className="allPlayer">
-            <h1>{p.name}</h1>
-        </div>)}
+        {players.map((player, index) => 
+            <div key={index} className="allPlayers" 
+                onClick={() => navigate(`/${player.name}`)}>
+                <h1>{player.name}</h1>
+        </div>
+        )}
     </>
 }
